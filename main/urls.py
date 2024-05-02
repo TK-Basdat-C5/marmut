17b20bf868d1624c2a503eb0ccc01d490d46c79c
FaizAbdur <faizsocool@gmail.com>
2024-05-02 21:13:08 +0700

    hehe 1

--- main/urls.py
+++ main/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     path('register/label', register_label, name='register-label'),
     path('register/pengguna', register_pengguna, name='register-pengguna'),
     path('dashboard/', dashboard, name='dashboard'),
-]
+    path ('add_playlist/', add_playlist, name='add_playlist'),
+    path('add_song_playlist/', add_song_playlist, name='add_song_playlist'),
+    path('add_song/', add_song, name='add_song'),
+    path('new_userplaylist/', new_userplaylist, name='new_userplaylist'),
+    path('playlistform/', playlistform, name='playlistform'),
+    path('user_playlist_details/' , user_playlist_details, name='user_playlist_details'),
+    path('user_playlistDetails/', user_playlistDetails, name='user_playlistDetails'),
+    path('play_song/', play_song, name='play_song'),
+    ]
--- main/views.py
+++ main/views.py
@@ -23,3 +23,35 @@ def register_pengguna(request):
 def dashboard(request):
     context = {}
     return render(request, "dashboard.html", context)
+
+def add_playlist(request):
+    context = {}
+    return render(request, "add_playlist.html", context)
+
+def add_song_playlist(request):
+    context = {}
+    return render(request, "add_song_playlist.html", context)
+
+def add_song(request):
+    context = {}
+    return render(request, "add_song.html", context)
+
+def new_userplaylist(request):
+    context = {}
+    return render(request, "new_userplaylist.html", context)
+
+def playlistform(request):
+    context = {}
+    return render(request, "playlist_form.html", context)
+
+def user_playlist_details(request):
+    context = {}
+    return render(request, "user_playlist_details.html", context)
+
+def user_playlistDetails(request):
+    context = {}
+    return render(request, "user_playlistDetails.html", context)
+
+def play_song(request):
+    context = {}
+    return render(request, "play_song.html", context)
