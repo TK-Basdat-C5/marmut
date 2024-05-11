ce418d7875fd301df8b097bb8af272cc8351211b
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-05-11 15:05:11 +0700

    set urls.py

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    context = {}
+    return render(request, "login.html", context)
+
+def register(request):
+    context = {}
+    return render(request, "register.html", context)
+
+def register_label(request):
+    context = {}
+    return render(request, "register_label.html", context)
+
+def register_pengguna(request):
+    context = {}
+    return render(request, "register_pengguna.html", context)
--- main/views.py
+++ main/views.py
@@ -4,21 +4,6 @@ def landing(request):
     context = {}
     return render(request, "landing.html", context)
 
-def login(request):
-    context = {}
-    return render(request, "login.html", context)
-
-def register(request):
-    context = {}
-    return render(request, "register.html", context)
-
-def register_label(request):
-    context = {}
-    return render(request, "register_label.html", context)
-
-def register_pengguna(request):
-    context = {}
-    return render(request, "register_pengguna.html", context)
 
 def dashboard(request):
     context = {}
