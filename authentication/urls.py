1e3da9ea48b9d69cfc0b4ec58eeced39310b703d
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-05-12 15:43:42 +0700

    finished register

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from auth.views import *
+from authentication.views import *
 
-app_name = 'auth'
+app_name = 'authentication'
 
 urlpatterns = [
+    path('', show_landingpage, name='landing-page'),
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('register/label', register_label, name='register-label'),
--- marmut/settings.py
+++ marmut/settings.py
@@ -30,9 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'auth',
     'chart',
-    'podcast'
+    'podcast',
+    'authentication',
 ]
 
 MIDDLEWARE = [
