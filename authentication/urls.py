8c29ae3410f07a2393f4c7e0aed220004246a6ad
Abbilville <abbilville@gmail.com>
2024-05-12 19:07:09 +0700

    login dan navbar selesai

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', show_landingpage, name='landing-page'),
     path('login/', login, name='login'),
     path('register/', register, name='register'),
-    path('register/label', register_label, name='register-label'),
-    path('register/pengguna', register_pengguna, name='register-pengguna'),
+    path('logout/', logout, name="logout"),
+    path('register/label/', register_label, name='register-label'),
+    path('register/pengguna/', register_pengguna, name='register-pengguna'),
+    path('dashboard/', dashboard, name='dashboard')
 ]
