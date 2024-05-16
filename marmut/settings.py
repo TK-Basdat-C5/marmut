024c906fc21c30dbd450943e16bd75d2ea6a90cc
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-05-16 20:03:36 +0700

    nyoba deploy part sekian

--- marmut/settings.py
+++ marmut/settings.py
@@ -75,8 +75,8 @@ DATABASES = {
         'NAME': 'your_db_name',
         'USER': os.getenv('DB_USER', 'default_db_user'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'default_db_password'),
-        'HOST': 'your_db_host',
-        'PORT': 'your_db_port',
+        'HOST': "aws-0-ap-southeast-1.pooler.supabase.com",
+        'PORT': "5432",
     }
 }
 
